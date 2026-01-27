bee7479ad29793f341528aee19073d6b9028fbf2
chickenmcsussy-sketch <chickenmcsussy@gmail.com>
2026-01-27 12:24:49 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,40 @@
+const CACHE_NAME = "valentine-v1";
+const OFFLINE_URL = "offline.html";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./offline.html",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./cat1.gif",
+  "./cat2.gif",
+  "./penguin.jpg"
+];
+
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("valentine-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./cat1.gif",
-        "./cat2.gif",
-        "./penguin.jpg"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(response => {
+        return response || caches.match(OFFLINE_URL);
+      })
+    )
   );
 });
